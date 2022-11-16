314eedb81bb678704a540755c219d4d7a56284cc
Shinlor Li <73413064+ShinlorLi@users.noreply.github.com>
2022-11-16 20:42:07 +0800

    Update cover.vue

--- layouts/cover.vue
+++ layouts/cover.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <img src="https://isaacpic-1305775614.cos.ap-beijing.myqcloud.com/uPic/vatis.png" class="w-30 m-5">
-    <img src="https://isaacpic-1305775614.cos.ap-beijing.myqcloud.com/uPic/cuclogo.png" class="w-60 absolute top-10 right-5">
+    <img src="https://allen2001-1311287324.cos.ap-nanjing.myqcloud.com/pictures/%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20220914201035.jpg" class="w-60 absolute top-10 right-5">
     <div class="bg-[#052780]">
       <div class="slidev-layout cover my-22 text-white">
         <div class="my-auto w-full">
